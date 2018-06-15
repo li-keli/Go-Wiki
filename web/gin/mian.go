9d75d95cab1650430e1efb004291f30a39423be6
likeli <13120168539@163.com>
2018-06-15 18:14:10 +0800

    依附nginx部署web

--- web/gin/mian.go
+++ web/gin/mian.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/gin-gonic/gin"
 )
 
+// docker 部署
+// docker run -p 8081:80 --rm -d -v "$(pwd)/conf.d":/etc/nginx/conf.d -v "$(pwd)":/var/work -w /var/work nginx sh -c "nginx & ./gin"
 func main() {
 	fmt.Println("hello")
 	r := gin.Default()
