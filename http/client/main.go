c81dc19427baf517179636db73326b990711bb42
likeli <13120168539@163.com>
2018-06-28 15:17:54 +0800

    go上传文件客户端

---
+++ http/client/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+
+	"github.com/parnurzeal/gorequest"
+)
+
+var (
+	imgAdd    = "./test.jpeg"
+	serverUrl = "http://localhost:5200/imgcode"
+)
+
+func main() {
+	f, _ := filepath.Abs(imgAdd)
+	bytesOfFile, _ := ioutil.ReadFile(f)
+
+	_, info, _ := gorequest.New().Post(serverUrl).
+		Type("multipart").
+		SendFile(bytesOfFile, "img", "img").
+		End()
+
+	fmt.Println(info)
+}
