b3923cb5fe3977b167a33a7ab3717411d896e8cb
likeli <13120168539@163.com>
2018-06-28 15:20:22 +0800

    update rename

--- http/client/main.go
+++ http/client/main.go
@@ -10,14 +10,14 @@ import (
 
 var (
 	imgAdd    = "./test.jpeg"
-	serverUrl = "http://localhost:5200/imgcode"
+	serverURL = "http://localhost:5200/imgcode"
 )
 
 func main() {
 	f, _ := filepath.Abs(imgAdd)
 	bytesOfFile, _ := ioutil.ReadFile(f)
 
-	_, info, _ := gorequest.New().Post(serverUrl).
+	_, info, _ := gorequest.New().Post(serverURL).
 		Type("multipart").
 		SendFile(bytesOfFile, "img", "img").
 		End()
