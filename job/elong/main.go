56a65f9ab451cb234219313624f4d1261e2890a7
likeli <13120168539@163.com>
2018-07-03 15:39:49 +0800

    job success

--- job/elong/main.go
+++ job/elong/main.go
@@ -2,29 +2,37 @@ package main
 
 import (
 	"log"
+	"sync"
+	"time"
 )
 
 var (
+	wait    sync.WaitGroup
 	mysqlDb DbMysql
 )
 
 func main() {
 	var hotels = readAllHotelId()
 	go mysqlDb.init()
+	time.Sleep(3 * time.Second)
 
 	go do(hotels[:30000])
 	go do(hotels[30000:60000])
 	go do(hotels[60000:])
 
 	log.Println("wating...")
-	select {}
+	wait.Wait()
+
+	log.Println("job 完成")
 }
 
 func do(hotels []Hotel) {
+	wait.Add(1)
 	for index, hotel := range hotels {
-		log.Printf("检索索引 -> %d", index)
+		log.Printf("检索索引 -> %d, 酒店编号 -> %d", index, hotel.HotelId)
 		getHotelDetail(hotel)
 	}
+	wait.Done()
 }
 
 func checkError(info string, args ...interface{}) {
